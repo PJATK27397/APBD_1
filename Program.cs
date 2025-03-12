50d0d7c3958a7b0e66ffec8a8253ab8a69d689b4
Mateusz Jastrzębski <s27397@pjwstk.edu.pl>
2025-03-13 00:59:46 +0100

    Dodano metodę CalculateAverage

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hellooo, World!!!");
 Console.WriteLine("21");
+
+int[] numbers = {1, 2, 3, 4, 5};
+Console.WriteLine("Average: " + CalculataAverage(numbers));
+
+static double CalculataAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        throw new ArgumentException("Tablica nie może być pusta.");
+
+    double sum = 0;
+    foreach (int num in numbers)
+    {
+        sum += num;
+    }
+
+    return sum / numbers.Length;
+}
